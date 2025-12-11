efcfc66aee7ed3bb8844b9ec720e11711a3e856c
Jonathan Cross <jonathan.a.cross@gmail.com>
2025-12-10 22:07:12 -0800

    arrows do damage now!

--- roguerpg/arrow.go
+++ roguerpg/arrow.go
@@ -8,6 +8,7 @@ type Arrow struct {
 	spriteSheet           *SpriteSheet
 	remainingFlightFrames int
 	velocity              Vector
+	damageSource          DamageSourceConfig
 }
 
 const (
@@ -26,17 +27,27 @@ func NewArrow(location Location, velocity Vector) *Arrow {
 	spriteSheet := NewSpriteSheet(16, 16, 4, 1)
 	var spriteSheetDir int
 	direction := VectorToDirection(velocity)
+	hitBox := Rect{}
 	switch direction {
 	case Up:
 		spriteSheetDir = arrowUpIndex
+		hitBox = Rect{Left: -2, Top: -8, Right: 2, Bottom: 0}
 	case Down:
 		spriteSheetDir = arrowDownIndex
+		hitBox = Rect{Left: -2, Top: 0, Right: 2, Bottom: 8}
 	case Left:
 		spriteSheetDir = arrowLeftIndex
+		hitBox = Rect{Left: -8, Top: -2, Right: 0, Bottom: 2}
 	case Right:
 		spriteSheetDir = arrowRightIndex
+		hitBox = Rect{Left: 0, Top: -2, Right: 8, Bottom: 2}
 	}
 	remainingFlightFrames := int(math.Round(arrowMaxDistance / arrowSpeed))
+
+	damageSource := DamageSourceConfig{
+		HitBox: hitBox,
+		Damage: 1,
+	}
 
 	return &Arrow{
 		BaseSprite: BaseSprite{
@@ -51,59 +62,13 @@ func NewArrow(location Location, velocity Vector) *Arrow {
 		spriteSheet:           spriteSheet,
 		remainingFlightFrames: remainingFlightFrames,
 		velocity:              velocity.Normalize().Scale(arrowSpeed),
+		damageSource:          damageSource,
 	}
 }
 
 func (b *Arrow) Update(level *Level, _ *Player) UpdateResult {
 	b.Location = Location(Vector(b.Location).Plus(b.velocity))
 	b.remainingFlightFrames--
-
-	return UpdateResult{}
-}
-
-func (b *Arrow) CanRemove() bool {
-	return b.remainingFlightFrames <= 0
-}
-
-type ArrowExplosion struct {
-	BaseSprite
-	spriteSheet    *SpriteSheet
-	animation      *Animation
-	finished       bool
-	attackHitboxes map[int]DamageSourceConfig
-}
-
-func NewArrowExplosion(location Location) *ArrowExplosion {
-	animation := NewAnimation([]int{0, 1, 2, 3, 4, 5}, 5, false)
-	spriteSheet := NewSpriteSheet(48, 48, 6, 1)
-
-	attackHitboxes := map[int]DamageSourceConfig{
-		1: {HitBox: Rect{Left: -24, Top: -24, Right: 24, Bottom: 24}, Damage: 2},
-	}
-
-	return &ArrowExplosion{
-		BaseSprite: BaseSprite{
-			Location: location,
-			image:    ArrowSpritesImage,
-			srcRect:  spriteSheet.Rect(0),
-			drawOffset: Location{
-				X: 24,
-				Y: 24,
-			},
-		},
-		spriteSheet:    spriteSheet,
-		animation:      animation,
-		finished:       false,
-		attackHitboxes: attackHitboxes,
-	}
-}
-
-func (b *ArrowExplosion) Update(level *Level, _ *Player) UpdateResult {
-	b.animation.Update()
-	b.srcRect = b.spriteSheet.Rect(b.animation.Frame())
-	if b.animation.IsFinished() {
-		b.finished = true
-	}
 
 	if ds := b.getActiveDamageSource(); ds != nil {
 		return UpdateResult{Actions: []Action{{
@@ -114,17 +79,11 @@ func (b *ArrowExplosion) Update(level *Level, _ *Player) UpdateResult {
 	return UpdateResult{}
 }
 
-func (b *ArrowExplosion) CanRemove() bool {
-	return b.finished
+func (b *Arrow) CanRemove() bool {
+	return b.remainingFlightFrames <= 0
 }
 
-func (b *ArrowExplosion) getActiveDamageSource() *DamageSource {
-	animIndex := b.animation.frameIndex
-
-	if config, ok := b.attackHitboxes[animIndex]; ok {
-		worldHitbox := config.HitBox.Offset(b.X, b.Y)
-		return NewDamageSource(TagPlayer, worldHitbox, config.Damage)
-	}
-
-	return nil
+func (b *Arrow) getActiveDamageSource() *DamageSource {
+	worldHitbox := b.damageSource.HitBox.Offset(b.X, b.Y)
+	return NewDamageSource(TagPlayer, worldHitbox, b.damageSource.Damage)
 }
